110b25acd6fe0d70f9f0c2e1553c1c864c8d90a6
Joey Chen <joeychen@yahoo-inc.com>
2015-05-19 14:03:53 +0800

    compare person with specflow table.CompareToInstance()

---
+++ ComparingObjectInTest/FindPerson.feature
@@ -0,0 +1,10 @@
+﻿Feature: FindPerson	
+
+
+Scenario: Compare 2 Person instance Equals
+	When I got a acutal person
+	| Id | Name | Age |
+	| 1  | A    | 10  |
+	Then I hope actual person should be equal to expected person
+	| Id | Name | Age |
+	| 1  | A    | 10  |
---
+++ ComparingObjectInTest/FindPersonSteps.cs
@@ -0,0 +1,23 @@
+﻿using TechTalk.SpecFlow;
+//we need table's extension method of TechTalk.SpecFlow.Assist
+using TechTalk.SpecFlow.Assist;
+namespace ComparingObjectInTest
+{
+    [Binding]
+    public class FindPersonSteps
+    {
+        [When(@"I got a acutal person")]
+        public void WhenIGotAAcutalPerson(Table table)
+        {
+            var actual = table.CreateInstance<Person>();
+            ScenarioContext.Current.Set<Person>(actual);
+        }
+
+        [Then(@"I hope actual person should be equal to expected person")]
+        public void ThenIHopeActualPersonShouldBeEqualToExpectedPerson(Table expected)
+        {
+            var actual = ScenarioContext.Current.Get<Person>();
+            expected.CompareToInstance<Person>(actual);
+        }
+    }
+}
